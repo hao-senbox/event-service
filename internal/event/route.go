f3ab83416045d0b0299d961deb922a3f2b39d5d5
HaoVo2007 <hao01638081724@gmail.com>
2025-08-18 16:25:37 +0700

    push code

--- internal/event/route.go
+++ internal/event/route.go
@@ -14,8 +14,8 @@ func RegisterRoutes(r *gin.Engine, handler *EventHandler) {
 		eventGroup.GET("/:id", handler.GetEventByID)
 		eventGroup.PUT("/:id", handler.UpdateEvent)
 		eventGroup.DELETE("/:id", handler.DeleteEvent)
-		eventGroup.PUT("toggle-send/:id", handler.ToggleSendEventNotifications)
-		eventGroup.PUT("toggle-show/:id", handler.ToggleShowEventNotifications)
+		eventGroup.PUT("/toggle-send/:id", handler.ToggleSendEventNotifications)
+		eventGroup.PUT("/toggle-show/:id", handler.ToggleShowEventNotifications)
 		eventGroup.POST("/trigger", handler.SendEventNotifications)
 	}
 }
